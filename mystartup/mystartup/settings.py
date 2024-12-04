91d111267e31ceb443647861da7cdd5c338e8a08
therajesh1 <therajeshchheda@gmail.com>
2024-12-04 19:48:41 +0530

    A

--- mystartup/mystartup/settings.py
+++ mystartup/mystartup/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this middleware
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
